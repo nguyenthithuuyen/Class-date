2690dd7e4e0d4d170b8a295dcf174b34a85914d8
uyen <uyen@codegym.vn>
2020-10-09 09:10:20 +0700

    nộp bài

---
+++ js/Date.js
@@ -0,0 +1,20 @@
+let MyDate = function(day, month, year) {
+
+    this.day = day;
+
+    this.month = month;
+
+    this.year = year;
+}
+    this.setDay = function (days) {
+        day = days;
+    };
+    this.getDay = function () {
+        return day
+    };
+    this.getMonth = function () {
+        return month;
+    };
+    this.getYear = function () {
+        return year
+    };
